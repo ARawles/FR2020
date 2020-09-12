8f9a2c967d8623d9642afb7d9e8596952515867a
ARawles <adamrawles@hotmail.co.uk>
2020-09-12 11:38:44 +0100

    Added script

--- _main.R
+++ _main.R
@@ -1,3 +1,4 @@
 library(rmarkdown)
 library(xaringan)
 rmarkdown::render("presentation.Rmd", output_file = "index.html", output_dir = "docs")
+rmarkdown::render("script.Rmd", output_file = "script.html", output_dir = "docs")
