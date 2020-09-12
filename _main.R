1da99ca59a7c9efb41da9332a30278b85fe638e2
ARawles <adamrawles@hotmail.co.uk>
2020-09-12 11:17:01 +0100

    Changed output to "index.html" file to make hosting easier

--- _main.R
+++ _main.R
@@ -1,3 +1,3 @@
 library(rmarkdown)
 library(xaringan)
-rmarkdown::render("presentation.Rmd", output_dir = "docs")
+rmarkdown::render("presentation.Rmd", output_file = "index.html", output_dir = "docs")
